762a1adf28ce17ae8c9aca030fd6a725ddc67e3c
Jon Perkins <captainperk81@gmail.com>
2018-05-01 20:25:55 -0400

    Creating Read-Only Auto-Implemented Properties

--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -2,10 +2,16 @@
 {
     public class Product
     {
+        public Product(bool stock = true)
+        {
+            InStock = stock;
+        }
+
         public string Name { get; set; }
         public string Category { get; set; } = "Watersports";
         public decimal? Price { get; set; }
         public Product Related { get; set; }
+        public bool InStock { get; };
 
         public static Product[] GetProducts()
         {
@@ -16,7 +22,7 @@
                 Price = 275M
             };
 
-            var lifejacket = new Product
+            var lifejacket = new Product(false)
             {
                 Name = "Lifejacket",
                 Price = 48.95M
